ac610e5ecb9b84db5c0f2fe646d8cd16c7b02065
vimalkrishnan <vimal_viyookaran@yahoo.co.in>
2019-05-02 09:55:19 +0530

    Api has been integrated

---
+++ App_Code/UpdateResponseModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for UpdateResponseModel
+/// </summary>
+public class UpdateResponseModel
+{
+    public string ErrorCode { get; set; }
+    public bool AppUpdate { get; set; }
+    public bool ForceUpdate { get; set; }
+
+    public UpdateResponseModel()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
---
+++ App_Code/VersionController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+/// <summary>
+/// Summary description for VersionController
+/// </summary>
+[RoutePrefix("api/version")]
+public class VersionController : ApiController
+{
+    public VersionController()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    [HttpGet]
+    [Route("{version}")]
+    public UpdateResponseModel GetVersionDetails([FromUri]string version)
+
+    {
+        return new UpdateResponseModel { ForceUpdate = false, AppUpdate = false, ErrorCode = "0" };
+    }
+}
