f2b0173a645db918149da360ca22a30552904ebe
vimalkrishnan <vimal_viyookaran@yahoo.co.in>
2019-05-02 23:21:15 +0530

    versioning logic has been added

--- App_Code/VersionController.cs
+++ App_Code/VersionController.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+using System.Data.SqlClient;
+using System.Net.Http;
+using System.Net;
+using System.Net.Http.Formatting;
 
 /// <summary>
 /// Summary description for VersionController
@@ -19,9 +24,37 @@ public class VersionController : ApiController
 
     [HttpGet]
     [Route("{version}")]
-    public UpdateResponseModel GetVersionDetails([FromUri]string version)
-
+    public HttpResponseMessage GetVersionDetails([FromUri]string version)
     {
-        return new UpdateResponseModel { ForceUpdate = false, AppUpdate = false, ErrorCode = "0" };
+        if (string.IsNullOrWhiteSpace(version))
+        {
+            return new HttpResponseMessage(HttpStatusCode.BadRequest)
+            {
+                Content = new StringContent("Version number should not be empty! ")
+            };
+        }
+        string sql = "Select * from versionSettings where versionNumber=@versionNumber";
+        DataTable dataTable = DataAccess.FillDataSet(sql, CommandType.Text, new SqlParameter[] { new SqlParameter("@versionNumber", version) }).Tables[0];
+
+        if (dataTable.Rows.Count == 0)
+        {
+            return new HttpResponseMessage(HttpStatusCode.BadRequest)
+            {
+                Content = new StringContent("Invalid version number! ")
+            };
+        }
+        return new HttpResponseMessage(HttpStatusCode.OK)
+        {
+            Content = new ObjectContent(
+                typeof(UpdateResponseModel),
+                new UpdateResponseModel
+                {
+                    ForceUpdate = Convert.ToBoolean(dataTable.Rows[0]["ForceUpdate"]),
+                    AppUpdate = Convert.ToBoolean(dataTable.Rows[0]["AppUpdate"]),
+                    ErrorCode = Convert.ToString(dataTable.Rows[0]["ErrorCode"])
+                },
+                new JsonMediaTypeFormatter())
+
+        };
     }
 }
---
+++ Sql/VersionSettings.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[VersionSettings](
+	[VersionId] [int] IDENTITY(1,1) NOT NULL,
+	[VersionNumber] [nvarchar](50) NULL,
+	[AppUpdate] [bit] NULL,
+	[ForceUpdate] [bit] NULL,
+	[ErrorCode] [nvarchar](50) NULL,
+ CONSTRAINT [PK_VersionSettings] PRIMARY KEY CLUSTERED 
+(
+	[VersionId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
